7249eb2c6f27430735a1ee5ff7de59dbea3dc411
Muhammad Bayu Nugraha <bnugraha00@gmail.com>
2016-10-03 22:18:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <!-- 
-// BayChat ..
-// Versi : 2.1
-// Created : 29 Jul 2016
-// Updated : 27 Sep 2016
+// BChat .. http://chat.bayyu.me
+// Versi : 2.0 (BETA)
+// Created : 2 Agust 2016
+// Updated : 3 Okt 2016
 -->
+
 <!DOCTYPE html>
 <html>
    <head> 
@@ -40,24 +41,24 @@
   <img src="http://chat.bayyu.me/image/BChat.png" height="70px">
   <h3>Siapa Nama Anda?</h3><br>
     <input type="text" minlength="4" required="" class="user-text" id="ses_user" maxlength="12"><br><br>
-    <button type="button" id="masuk" class="btn-primary">Mulai Chat</i>
+     <button type="button" id="masuk" class="btn-primary">Mulai Chat</button>
   </div>
 </div>
 <div id="wrapper" class="konten">
   <div class="container">
     <div class="row">
       <div id="pesan_nya">
-          <div id="typ"></div>
+          
       </div>
     </div> <!-- / Konten ISI -->
   </div>
 </div>
-<div class="kirim_pesan" id="msg_form">
+ 
+      <!-- // UNTUK INPUT PESAN -->
 <input type="text" placeholder="Masukan pesan..." maxlength="850" id="message" class="text-pesan">
-<input type="hidden" id="user" value="<?php echo $_SERVER['REMOTE_ADDR']; ?>" />
+<input type="hidden" id="user" value="<?php echo $_SERVER['REMOTE_ADDR']; ?>" /> 
 <input type="hidden" id="alamat" value="<?php echo $_SERVER['REMOTE_ADDR']; ?>" />
  
-</div>
      <!-- jQuery -->
      <script src="//code.jquery.com/jquery-2.1.3.min.js"></script>
      <!-- Lib -->
